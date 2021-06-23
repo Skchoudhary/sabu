e58b4ebaa72fcfb8e76e83f926424a6310ba4490
Sandeep Choudhary <sandeepchoudhary1507@gmail.com>
2021-06-23 18:55:48 +0530

    notification creation logic

--- sabusrc/__init__.py
+++ sabusrc/__init__.py
@@ -1,47 +1,59 @@
-import yaml
+import json
+import time
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from pynotifier import Notification
 
-TASKS_PATH = "sabusrc/tasks/task.yml"
+TASKS_PATH = "/tasks/task.json"
 
 
-def current_day() -> str:
-    return datetime.now().today().strftime("%A")
+class Sabu:
+    def current_day(self) -> str:
+        return datetime.now().today().strftime("%A").lower()
 
-
-def read_tasks(file: str = TASKS_PATH):
-    with open(file, "r") as stream:
-        try:
-            print(yaml.safe_load(stream))
-        except yaml.YAMLError as exc:
-            print(exc)
-
-
-class NotificationHandler:
-    def __init__(self) -> None:
-        pass
-
-    def create_notification(
-        title: str,
-        desc: str,
-        icon_path: str = "/absolute/path/to/image/icon.png",
-        duration: int = 5,
-    ) -> None:
+    def get_tasks(self, file: str = TASKS_PATH) -> list:
         """
-        create the system notifications
+        Read the task from json file
         """
-        Notification(
-            title=title,
-            description=desc,
-            icon_path=icon_path,
-            duration=duration,
-            urgency="normal",
-        ).send()
+        with open(file, "r") as stream:
+            try:
+                data = json.load(stream)
+                day_name = self.current_day()
+                tasks = data["all"] + data[day_name]
+                task_times_wise = {}
+                for task in tasks:
+                    if task["time"] in task_times_wise:
+                        task_times_wise[task["time"]].append({"task": task["task"]})
+                    else:
+                        task_times_wise[task["time"]] = [{"task": task["task"]}]
+            except Exception as exc:
+                task_times_wise = []
+                print(exc)
+        return task_times_wise
+
+    def create_task_notification(self):
+        """ """
+        tasks = self.get_tasks()
+        while True:
+            current_time = datetime.now().strftime("%H:%M")
+            starttime = time.time()
+            print("firsdt: ", tasks)
+            if current_time in tasks:
+                for task in tasks[current_time]:
+                    Notification(
+                        title=task["task"],
+                        description=task["task"],
+                        icon_path="/absolute/path/to/image/icon.png",
+                        duration=5,
+                        urgency="normal",
+                    ).send()
+            # sleep for a minute        
+            time.sleep(60)
 
 
 def main():
-    pass
+    sabu = Sabu()
+    sabu.create_task_notification()
 
 
 if __name__ == "__main__":
